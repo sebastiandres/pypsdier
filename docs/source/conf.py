060bc9bece80caec535e13554417919cbf9eabf7
Sebastian Flores <sebastiandres@Sebastians-MacBook-Pro.local>
2020-07-20 17:09:19 -0400

    Fixing the conf.py in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../GenericSimulationLibrary/'))
+sys.path.insert(0, os.path.abspath('../../pypsdier/'))
 
 # -- Project information -----------------------------------------------------
 
